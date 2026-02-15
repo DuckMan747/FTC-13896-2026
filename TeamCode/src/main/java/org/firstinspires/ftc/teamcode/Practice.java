17e69f55bfa88af7ba953263c2e17c20b91a1a08
NOAAAfterDark <texasduck47@gmail.com>
2026-02-15 11:16:06 -0600

    Added Basic Pedro Constants
    Just added pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Practice")
+public class Practice extends OpMode {
+
+    String Hello = "Nice";
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
